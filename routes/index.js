dea85cac377666548976d774263854340ca9237c
Juan Lavallen <juanlavallen02@gmail.com>
2022-04-18 15:17:25 -0300

    ruta index terminada

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const PaymentController = require('../controllers/PaymentController');
+const PaymentService = require('../services/Payments.Service');
+
+const PaymentInstance = new PaymentController(new PaymentService());
+
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/payment', (req, res, next) => {
+  PaymentInstance.getPaymentLink(req, res);
+});
+
+router.get('/subscription', (req, res, next) => {
+  PaymentInstance.getSubscriptionLink(req, res);
 });
 
 module.exports = router;
